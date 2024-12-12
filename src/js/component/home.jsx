57b181748fbc0c0acbff21e10fe3a580bf66b5ff
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2024-12-12 11:27:58 +0000

    Semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
-import React from "react";
 
+import React from "react";
+import { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
+
 
 //create your first component
-const Home = () => {
+export function Home() {
+	const [colorSemaforo, setcolorSemaforo] = useState("red");
+	const colorAleatorio =["red", "yellow", "green"]
+	let colorSeleccion= colorAleatorio[Math.floor(Math.random()*3)]
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	<div className="container d-flex align-items-center justify-content-center">
+		<button className="btn btn-primary" onClick= {()=>setcolorSemaforo(colorSeleccion)}  style={{width:"150px", height: "50px"}} >Change the color</button>
+		<div className="Semaforo bg-black p-2 m-auto d-inline-block">
+			<div className={`luz mb-1 bg-danger ${colorSemaforo === "red" ? "glow" : ""}`} onClick={() => setcolorSemaforo("red")}></div>
+			<div className={`luz mb-1 bg-warning ${colorSemaforo === "yellow" ? "glow" : ""}`} onClick={() => setcolorSemaforo("yellow")}></div>
+			<div className={`luz mb-1 bg-success ${colorSemaforo === "green" ? "glow" : ""}`} onClick={() => setcolorSemaforo("green")}></div>
 		</div>
-	);
-};
+	</div>);
+}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<Home/>);
 
